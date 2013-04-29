f13e71f91501c8527f1e1bdcf81e9857d2c7471c
Andy Hamilton <andyhmltn@gmail.com>
2013-04-29 10:01:37 +0100

    Added proper error handling

--- sync.py
+++ sync.py
@@ -21,4 +21,5 @@ if(request.ok):
 		print "Cloning repo: " + str(repo['full_name'])
 		os.system("git clone " + repo_url)
 else:
-	print request
+	print "There was an error with your request"
+	print "Error code:"+ str(request.status_code)
